c795112b6d9ccbc4a03c72875ab40cb315d0e840
Yanbin Qiu <yanbin.qiu@morningstar.com>
2022-07-05 09:16:49 -0500

    jwt token supports ROLE

--- src/main/java/yanbin/blog/JwtAuthentication.java
+++ src/main/java/yanbin/blog/JwtAuthentication.java
@@ -1,15 +1,33 @@
 package yanbin.blog;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 public class JwtAuthentication extends AbstractAuthenticationToken {
 
-    private final Object principal;
+    private final DecodedJWT decodedJWT;
 
-    public JwtAuthentication(Object principal) {
+    public JwtAuthentication(DecodedJWT decodedJWT) {
         super(null);
-        this.principal = principal;
-        setAuthenticated(true);
+        this.decodedJWT = decodedJWT;
+        setAuthenticated(decodedJWT.getExpiresAt().after(new Date()));
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return Optional.ofNullable(decodedJWT.getClaim("roles").asList(String.class))
+                .map(roles -> roles.stream().map(role -> (GrantedAuthority) new SimpleGrantedAuthority(role))
+                        .collect(toList()))
+                .orElse(emptyList());
     }
 
     @Override
@@ -19,6 +37,6 @@ public class JwtAuthentication extends AbstractAuthenticationToken {
 
     @Override
     public Object getPrincipal() {
-        return principal;
+        return decodedJWT;
     }
 }
--- src/main/java/yanbin/blog/SecurityConfig.java
+++ src/main/java/yanbin/blog/SecurityConfig.java
@@ -2,9 +2,12 @@ package yanbin.blog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -12,8 +15,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.authorizeHttpRequests().antMatchers("/public-api").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().hasRole("contributors");
 
+        httpSecurity.exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
         httpSecurity.addFilterBefore(new JwtTokenFilter(), AuthorizationFilter.class);
         return httpSecurity.build();
     }
